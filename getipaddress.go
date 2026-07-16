b14db0e54797d23a722a87a802164173fbce12b1
6gDav <c6gdavprog0402@gmail.com>
2026-07-16 14:36:00 +0200

    refactor: ip fetcher moved to another file

---
+++ getipaddress.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func getIPAddress() (net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("no active non-loopback IPv4 address found")
+}
--- main.go
+++ main.go
@@ -61,20 +61,3 @@ func main() {
 	<-sigChan
 	fmt.Println("\nShutting down server...")
 }
-
-func getIPAddress() (net.IP, error) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, address := range addrs {
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP, nil
-			}
-		}
-	}
-
-	return nil, fmt.Errorf("no active non-loopback IPv4 address found")
-}
