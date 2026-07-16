75049a0e8bf86c14094ad00a7dacf7f5aa554674
6gDav <c6gdavprog0402@gmail.com>
2026-07-16 11:53:09 +0200

    feat: page hosting is working

--- main.go
+++ main.go
@@ -2,42 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hashicorp/mdns"
 )
 
+const port = 3000
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./hosted/build")))
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./hosted/build"))
 
-	ipadress, err_ip := getIPAddress()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fileServer.ServeHTTP(w, r)
+	})
 
-	if err_ip != nil {
-		fmt.Println("Error occured ", err_ip)
-	}
+	go func() {
+		fmt.Printf("Server is running on port %d\n", port)
+		err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+		if err != nil {
+			log.Fatalf("Error occurred while trying to start the server: %v", err)
+		}
+	}()
 
-	concatedIp := ipadress + ":3000"
-
-	fmt.Printf("Server is running on http://%s\n", concatedIp)
-	err := http.ListenAndServe(concatedIp, nil)
-
+	ipAddress, err := getIPAddress()
 	if err != nil {
-		fmt.Println("Error occured while trying to start the server")
+		log.Fatalf("Error occurred while trying to fetch IP address: %v", err)
 	}
+
+	service, err := mdns.NewMDNSService(
+		"loginpage",
+		"_http._tcp",
+		"local.",
+		"loginpage.local.",
+		port,
+		[]net.IP{ipAddress},
+		[]string{"txtv=1"},
+	)
+	if err != nil {
+		log.Fatalf("Establish mDNS service is unsuccessful: %v", err)
+	}
+
+	server, err := mdns.NewServer(&mdns.Config{Zone: service})
+	if err != nil {
+		log.Fatalf("Error occurred while trying to start the mDNS server: %v", err)
+	}
+	defer server.Shutdown()
+
+	fmt.Printf("Web page is here: http://loginpage.local:%d\n", port)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	fmt.Println("\nShutting down server...")
 }
 
-func getIPAddress() (string, error) {
+func getIPAddress() (net.IP, error) {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	for _, address := range addrs {
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String(), nil
+				return ipnet.IP, nil
 			}
 		}
 	}
 
-	return "", fmt.Errorf("no active non-loopback IPv4 address found")
+	return nil, fmt.Errorf("no active non-loopback IPv4 address found")
 }
