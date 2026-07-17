b520e7ebd9be7304238153d1d0fad25a8d6763c4
6gDav <c6gdavprog0402@gmail.com>
2026-07-17 15:26:18 +0200

    feat: Fyne package added

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "hosting_login_page/server"
+import (
+	"hosting_login_page/server"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 	server.SetAPIendpoint()
 	server.SetMDNSserver()
+
 }
