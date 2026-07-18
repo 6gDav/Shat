6da8f2603163191ae287c29ddd3b882595ffe393
6gDav <c6gdavprog0402@gmail.com>
2026-07-18 17:00:58 +0200

    feat: page update but the server is useless

--- hosted/src/routes/+page.svelte
+++ hosted/src/routes/+page.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+
+    let { data }: { data: PageData } = $props();
+</script>
 <h1>Hello in the Login page</h1>
+<input type="text" placeholder="Enter your user name here"> <br>
+<input type="text" disabled>
+<p>Your IP adress <strong>{data.ip}</strong></p>
