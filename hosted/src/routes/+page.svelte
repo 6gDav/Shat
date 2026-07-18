0b01c06863fb90d2eeb2e4493094a19897f3380a
6gDav <c6gdavprog0402@gmail.com>
2026-07-18 17:18:56 +0200

    feat: client side API address fetch implemented but not good because of the NAT

--- hosted/src/routes/+page.svelte
+++ hosted/src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+import { onMount } from 'svelte';
+    
+    let userIP = $state("Betöltés...");
 
-
-    let { data }: { data: PageData } = $props();
+    onMount(async () => {
+        try {
+            const res = await fetch('https://api.ipify.org?format=json');
+            const data = await res.json();
+            userIP = data.ip;
+        } catch (e) {
+            userIP = "Error occured";
+        }
+    });
 </script>
 <h1>Hello in the Login page</h1>
 <input type="text" placeholder="Enter your user name here"> <br>
 <input type="text" disabled>
-<p>Your IP adress <strong>{data.ip}</strong></p>
+<p>Your IP adress <strong>{userIP}</strong></p>
