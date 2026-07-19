cf51c68837469b30c33c96538b78eaac9e0a92e7
6gDav <c6gdavprog0402@gmail.com>
2026-07-19 14:58:00 +0200

    feat: regsitration button and a basic logic added

--- hosted/src/routes/+page.svelte
+++ hosted/src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
     
+    let nameText = $state<string>()
+
+    function registrateName() {
+        console.log("Hello Wolrd" + nameText)
+    }
 </script>
+
 <h1>Hello in the Login page</h1>
-<input type="text" placeholder="Enter your user name here"> <br>
+<input type="text" placeholder="Enter your user name here" bind:value={nameText}> 
+<br> <br>
+<button onclick={registrateName}>Registrate</button>
