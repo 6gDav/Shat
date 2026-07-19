22170dc0bd45f20aaf6d5bd60d3b41f600c68f6d
6gDav <c6gdavprog0402@gmail.com>
2026-07-19 15:38:33 +0200

    feat: fronteden post logic added

--- hosted/src/routes/+page.svelte
+++ hosted/src/routes/+page.svelte
@@ -3,7 +3,26 @@
     let nameText = $state<string>()
 
     async function registrateName() {
+        try {
+            const response = await fetch("http://loginpage.local:3000/submit", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: nameText
+            });
 
+            if (!response.ok) {
+                throw new Error("POST resoult is not Ok")
+            } else {
+                console.log("Registration is succesfull")
+                //The registration is succes full. We will navigate to a nother file
+                //Navigation logic here
+            }
+
+        } catch (error) {
+            console.error("Error occured why trying to send your name: " + error)
+        }
     }
 </script>
 
