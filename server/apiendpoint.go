7f1aa94027277330d6bb94bac5067cfa7a6d04a2
6gDav <c6gdavprog0402@gmail.com>
2026-07-22 16:02:40 +0200

    docs: refactoration and debug todo added to the code as a comment

--- server/apiendpoint.go
+++ server/apiendpoint.go
@@ -69,6 +69,7 @@ func SetAPIendpoint() {
 	})
 
 	//HandShaking
+	//! Refactor this to the chat folder and solve the error problem (HTTP required WS)
 	mux.HandleFunc("GET /setws", func(w http.ResponseWriter, r *http.Request) {
 		ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 
