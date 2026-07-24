c18e2dadf491caa80e507c4f7248537331a9a54c
6gDav <c6gdavprog0402@gmail.com>
2026-07-24 14:24:55 +0200

    feat: mDNS server connected to log

--- server/mdnsserver.go
+++ server/mdnsserver.go
@@ -2,23 +2,25 @@ package server
 
 import (
 	"fmt"
+	"hosting_login_page/logs"
 
-	"log"
 	"net"
 
+	"fyne.io/fyne/v2/widget"
 	"github.com/hashicorp/mdns"
 )
 
 var MdnsServer *mdns.Server
+var domainName string = "loginpage"
 
 func SetMDNSserver() {
 	ipAddress, err := getIPAddress()
 	if err != nil {
-		log.Fatalf("Error occurred while trying to fetch IP address: %v", err)
+		logs.Logs.Add(widget.NewLabel("Error occurred while trying to fetch the IP address: " + err.Error()))
 	}
 
 	service, err := mdns.NewMDNSService(
-		"loginpage",
+		domainName,
 		"_http._tcp",
 		"local.",
 		"loginpage.local.",
@@ -27,16 +29,18 @@ func SetMDNSserver() {
 		[]string{"txtv=1"},
 	)
 	if err != nil {
-		log.Fatalf("Establish mDNS service is unsuccessful: %v", err)
+		logs.Logs.Add(widget.NewLabel("Establish mDNS service is unsuccessful: " + err.Error()))
 	}
 
 	var errServer error
 	MdnsServer, errServer = mdns.NewServer(&mdns.Config{Zone: service})
 	if errServer != nil {
-		log.Fatalf("Error occurred while trying to start the mDNS server: %v", errServer)
+		logs.Logs.Add(widget.NewLabel("Error occurred while trying to start the mDNS server: " + err.Error()))
 	}
 
-	fmt.Printf("Web page is here: http://loginpage.local:%d\n", Port)
+	//fmt.Printf("Web page is here: http://loginpage.local:%d\n", Port)
+	msg := fmt.Sprintf("Web page is available on this link: http://%s.local:%d", domainName, Port)
+	logs.Logs.Add(widget.NewLabel(msg))
 
 	// sigChan := make(chan os.Signal, 1)
 	// signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
