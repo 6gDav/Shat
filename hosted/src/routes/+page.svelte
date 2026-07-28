4665a36eed49bf202ad5a9b5daffd8de78dd4747
6gDav <c6gdavprog0402@gmail.com>
2026-07-28 11:14:44 +0200

    feat: frontend user dissconnetion removed

--- hosted/src/routes/+page.svelte
+++ hosted/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import "../app.css";
     import LoginPage from "./pages/Login/+page.svelte"
-    import { deleteUserOnClose } from './components/removeUser.svelte.ts';
-
-    deleteUserOnClose();
 </script>
 
 <main>
