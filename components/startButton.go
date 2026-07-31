28a4c6f55bb3015919fa1a0e940a56c2c48f97d0
6gDav <c6gdavprog0402@gmail.com>
2026-07-31 11:27:37 +0200

    refactor/feat: server managger added and logic to clear the hasmap as well

--- components/startButton.go
+++ components/startButton.go
@@ -1,15 +1,10 @@
 package components
 
 import (
-	"context"
-	"fmt"
-	"hosting_login_page/chat"
 	"hosting_login_page/logs"
-	"hosting_login_page/server"
-	"time"
+	runmanagger "hosting_login_page/runManagger"
 
 	"fyne.io/fyne/v2/widget"
-	"github.com/gorilla/websocket"
 )
 
 func StartButtonElement() *widget.Button {
@@ -22,70 +17,14 @@ func StartButtonElement() *widget.Button {
 			logs.Logs.Add(widget.NewLabel("Servers stopped..."))
 			clicked = false
 			button.SetText("Start")
-			stopServer()
+			runmanagger.StopServer()
 		} else {
 			logs.Logs.Add(widget.NewLabel("Servers started..."))
 			clicked = true
 			button.SetText("Stop")
-			startServer()
+			runmanagger.StartServer()
 		}
 	})
 
 	return button
-}
-
-func startServer() {
-	server.SetAPIendpoint()
-	server.SetMDNSserver()
-}
-
-func stopServer() {
-	//Closing mDNS service
-	if server.MdnsServer != nil {
-		server.MdnsServer.Shutdown()
-		server.MdnsServer = nil
-		logs.Logs.Add(widget.NewLabel("mDNS Server successfully shot down..."))
-	}
-
-	//Closing WebSocket connections
-
-	// chat.ClientsMu.Lock()
-	// for _, client := range chat.Clients {
-	// 	if client.Conn != nil {
-	// 		client.Conn.Close()
-	// 		client.Conn = nil
-	// 	}
-	// 	fmt.Println("All WebSocket connections closed")
-	// }
-	// chat.ClientsMu.Unlock()
-
-	chat.ClientsMu.Lock()
-	for ip, client := range chat.Clients {
-		if client.Conn != nil {
-
-			client.Conn.WriteControl(
-				websocket.CloseMessage,
-				websocket.FormatCloseMessage(websocket.CloseGoingAway, "Server shutting down"),
-				time.Now().Add(time.Second),
-			)
-			client.Conn.Close()
-			client.Conn = nil
-			logs.Logs.Add(widget.NewLabel("Closing WebSocket connection on this ip adress: " + ip))
-		}
-	}
-	chat.ClientsMu.Unlock()
-
-	//Closing HTTP connections
-	if server.HttpServer != nil {
-		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-		defer cancel()
-
-		if err := server.HttpServer.Shutdown(ctx); err != nil {
-			errMsg := fmt.Sprintf("Error occured while trying to stop the server: %v", err)
-			logs.Logs.Add(widget.NewLabel(errMsg))
-		} else {
-			logs.Logs.Add(widget.NewLabel("HTTP serves succesfully shot down..  "))
-		}
-		server.HttpServer = nil
-	}
 }
---
+++ runManagger/managger.go
@@ -0,0 +1,73 @@
+package runmanagger
+
+import (
+	"context"
+	"fmt"
+	"hosting_login_page/chat"
+	"hosting_login_page/logs"
+	"hosting_login_page/server"
+	"time"
+
+	"fyne.io/fyne/v2/widget"
+	"github.com/gorilla/websocket"
+)
+
+func StartServer() {
+	server.SetAPIendpoint()
+	server.SetMDNSserver()
+}
+
+func StopServer() {
+	//Closing mDNS service
+	if server.MdnsServer != nil {
+		server.MdnsServer.Shutdown()
+		server.MdnsServer = nil
+		logs.Logs.Add(widget.NewLabel("mDNS Server successfully shot down..."))
+	}
+
+	//Closing WebSocket connections
+
+	// chat.ClientsMu.Lock()
+	// for _, client := range chat.Clients {
+	// 	if client.Conn != nil {
+	// 		client.Conn.Close()
+	// 		client.Conn = nil
+	// 	}
+	// 	fmt.Println("All WebSocket connections closed")
+	// }
+	// chat.ClientsMu.Unlock()
+
+	chat.ClientsMu.Lock()
+	for ip, client := range chat.Clients {
+		if client.Conn != nil {
+
+			client.Conn.WriteControl(
+				websocket.CloseMessage,
+				websocket.FormatCloseMessage(websocket.CloseGoingAway, "Server shutting down"),
+				time.Now().Add(time.Second),
+			)
+			client.Conn.Close()
+			client.Conn = nil
+			logs.Logs.Add(widget.NewLabel("Closing WebSocket connection on this ip adress: " + ip))
+		}
+	}
+	chat.ClientsMu.Unlock()
+
+	//Closing HTTP connections
+	if server.HttpServer != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		if err := server.HttpServer.Shutdown(ctx); err != nil {
+			errMsg := fmt.Sprintf("Error occured while trying to stop the server: %v", err)
+			logs.Logs.Add(widget.NewLabel(errMsg))
+		} else {
+			logs.Logs.Add(widget.NewLabel("HTTP serves succesfully shot down..  "))
+		}
+		server.HttpServer = nil
+	}
+
+	chat.ClientsMu.Lock()
+	clear(chat.Clients)
+	chat.ClientsMu.Unlock()
+}
