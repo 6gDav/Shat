89a8741d0cf0a9a58a06f279fd082b0a619a42bb
6gDav <c6gdavprog0402@gmail.com>
2026-08-18 14:33:16 +0200

    fix: root directy link to page fixed

--- server/logic/servepage.go
+++ server/logic/servepage.go
@@ -2,7 +2,7 @@ package logic
 
 import "net/http"
 
-var fileServer = http.FileServer(http.Dir("../page/build"))
+var fileServer = http.FileServer(http.Dir("./page/build"))
 
 func ServePage(w http.ResponseWriter, r *http.Request) {
 	fileServer.ServeHTTP(w, r)
