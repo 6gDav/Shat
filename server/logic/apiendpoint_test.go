f5dfd660d81f36bbe266abf946faa40efd560724
6gDav <c6gdavprog0402@gmail.com>
2026-08-19 15:48:33 +0200

    test: RestoreChatHistory tested

--- server/logic/apiendpoint_test.go
+++ server/logic/apiendpoint_test.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"encoding/json"
+	"hosting_login_page/history"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -23,7 +24,7 @@ func TestServePage(t *testing.T) {
 
 func TestRedirectUser(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	req.RemoteAddr = "192.0.2.1:1234"
+	req.RemoteAddr = "192.0.2.1:1234" //TEST-NET-1 an IP address for tests
 
 	rec := httptest.NewRecorder()
 
@@ -45,5 +46,50 @@ func TestRedirectUser(t *testing.T) {
 
 	if got.Redirect != false {
 		t.Errorf("Expected redirect to be false, got %v", got.Redirect)
+	}
+}
+
+func TestRestoreChatHistory(t *testing.T) {
+	testIP := "192.0.2.1"
+
+	history.ChatStoreMu.Lock()
+	history.ChatHistory = map[string][]history.ChatMessage{
+		"Group": {
+			{
+				Type:     "group",
+				FromIp:   "10.0.0.1",
+				Message:  "Group lorem ipsum",
+				RoomId:   "Group",
+				UserName: "Jhon Doe",
+			},
+		},
+		"192.168.1.200_192.168.1.201": {
+			{
+				Type:     "private",
+				FromIp:   "192.168.1.200",
+				Message:  "Private lorem ipsum",
+				RoomId:   "192.168.1.200_192.168.1.201",
+				UserName: "Jhon Doe",
+			},
+		},
+	}
+	history.ChatStoreMu.Unlock()
+
+	req := httptest.NewRequest(http.MethodGet, "/restore-history", nil)
+
+	req.RemoteAddr = testIP + ":12345"
+
+	rec := httptest.NewRecorder()
+
+	RestoreChatHistory(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Fatalf("Expected 200, got %d. Body: %s", rec.Code, rec.Body.String())
+	}
+
+	var responseList []history.ChatMessage
+	err := json.Unmarshal(rec.Body.Bytes(), &responseList)
+	if err != nil {
+		t.Fatalf("The response is not a the valid form: %v", err)
 	}
 }
